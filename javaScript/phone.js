f9166a34b35f23e9d36e183eda89b313e16f3b24
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-08-19 21:27:53 +0600

    updating cost with amount buttons is done

--- javaScript/phone.js
+++ javaScript/phone.js
@@ -2,10 +2,18 @@
 
 // Case amount increases as Plus button is clicked
 document.getElementById("btn-phone-plus").addEventListener("click", function () {
-    quantityAmountPlusButton("phone-number-field");
+    let totalPhoneQuantity = quantityAmountPlusButton("phone-number-field");
+    const totalPhoneCost = totalPhoneQuantity * 1219;
+
+    const phoneElement = document.getElementById("phone-cost");
+    return phoneElement.innerText = totalPhoneCost;
 })
 
 // case amoount decreases as Minus button is clicked
 document.getElementById("btn-phone-minus").addEventListener("click", function () {
-    quantityAmountMinusButton("phone-number-field");
+    let totalPhoneQuantity = quantityAmountMinusButton("phone-number-field");
+    const totalPhoneCost = totalPhoneQuantity * 1219;
+
+    const phoneElement = document.getElementById("phone-cost");
+    return phoneElement.innerText = totalPhoneCost;
 })
--- javaScript/phoneCase.js
+++ javaScript/phoneCase.js
@@ -2,12 +2,21 @@
 
 // Case amount increases as Plus button is clicked
 document.getElementById("btn-case-plus").addEventListener("click", function () {
-    quantityAmountPlusButton("case-number-field");
+    let totalCaseQuantity = quantityAmountPlusButton("case-number-field");
+    const totalCaseCost = totalCaseQuantity * 59;
+
+    const caseElement = document.getElementById("case-cost");
+    return caseElement.innerText = totalCaseCost;
+
 })
 
 // case amoount decreases as Minus button is clicked
 document.getElementById("btn-case-minus").addEventListener("click", function () {
-    quantityAmountMinusButton("case-number-field");
+    let totalCaseQuantity = quantityAmountMinusButton("case-number-field");
+    const totalCaseCost = totalCaseQuantity * 59;
+
+    const caseElement = document.getElementById("case-cost");
+    return caseElement.innerText = totalCaseCost;
 })
 
 
